dfdb04be51f1687bfe5b4a13156ba3fc8cba2a13
Gianna Smith <gnsmith127@gmail.com>
2021-12-30 12:05:41 -0500

    finished exercise

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace PracticeLinq
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var videoGames = new List<string>()
+            {
+                "Madden",
+                "Fortnite",
+                "Tetris",
+                "FIFA",
+                "Zelda"
+            };
+
+            var ordered = videoGames
+                .OrderByDescending(name => name.Length)
+                .ToList();
+
+           
+
+            foreach (var item in ordered)
+            {
+                Console.WriteLine(item);
+            }
+
+
+        }
+    }
+}
